8c0ff5d011ae8775fd1cbb6306e10fe1d95fa05a
G. Willson <greg@biomassiv.es>
2024-09-07 22:16:06 -0600

    Create process_new-video-transcript-or-description.py

---
+++ api/process_new-video-transcript-or-description.py
@@ -0,0 +1,11 @@
+// process_new-video-transcript-or-description.py
+// filename implies one thing, but this file is for helping to submit new data to the database
+
+//  local  data  file
+
+//  supabase
+
+//  vercel   postgres
+
+//  google sheets
+
